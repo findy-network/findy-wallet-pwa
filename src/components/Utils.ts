f76c71a0e3dc96f7e510aa9a7f932dcf98eebbc3
Ville Seeste <ville.seeste@op.fi>
2021-04-01 15:20:17 +0300

    parse names helpers

--- src/components/Utils.ts
+++ src/components/Utils.ts
@@ -8,9 +8,20 @@ Utils.toDateString = (str: string) => {
   return d.toLocaleDateString()
 }
 
-Utils.getSchemaName = (schemaId: string) => {
+Utils.parseSchemaName = (schemaId: string) => {
   var splitted = schemaId.split(':', 3)
-  return splitted[2]
+  if (splitted[2]) {
+    return splitted[2]
+  }
+  return schemaId
+}
+
+Utils.parseIssuer = (schemaId: string) => {
+  var splitted = schemaId.split(':', 5)
+  if (splitted[4]) {
+    return splitted[4]
+  }
+  return schemaId
 }
 
 function Utils() {}
