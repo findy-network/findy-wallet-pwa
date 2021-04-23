84236f6aacc7fd211132205cbc9dde68c733aea2
Ville Seeste <ville.seeste@op.fi>
2021-04-23 09:53:45 +0300

    credential box

--- src/components/AcceptableJob.tsx
+++ src/components/AcceptableJob.tsx
@@ -1,17 +1,25 @@
 import React, { ReactNode, useState } from 'react'
-import { Button, Box } from 'grommet'
+import { Button, Box, Paragraph } from 'grommet'
 import styled from 'styled-components'
 import { chat, colors } from '../theme'
 
 import { useMutation } from '@apollo/client'
 import { IJobNode, JobStatus } from './Types'
 import { RESUME_JOB_MUTATION } from './Queries'
+import { RotateRight } from 'grommet-icons'
 
 const Btn = styled(Button)`
   border: 1px solid;
   margin: 0.2rem;
   font-size: ${chat.buttonFontSize};
   color: ${colors.chatText};
+`
+const P = styled(Paragraph)`
+  padding: 1px 10px 1px 10px;
+  margin: 0;
+  font-size: ${chat.fontSize};
+  color: ${colors.smallText};
+  overflow: hidden;
 `
 
 type IProps = {
@@ -36,7 +44,7 @@ function AcceptableJob({ job, children, canAccept }: IProps) {
     <div>
       {children}
       {job.status === JobStatus.PENDING && (
-        <Box direction="row" pad="1rem">
+        <Box direction="row" pad="0 1rem 1rem">
           <Btn
             disabled={decline}
             onClick={() => doResume(false)}
@@ -47,6 +55,14 @@ function AcceptableJob({ job, children, canAccept }: IProps) {
             onClick={() => doResume(true)}
             label="Accept"
           ></Btn>
+        </Box>
+      )}
+      {job.status === JobStatus.WAITING && (
+        <Box pad="0 0 1rem 1rem" direction="row">
+          <Box animation="rotateRight">
+            <RotateRight />
+          </Box>
+          <P>Processing</P>
         </Box>
       )}
     </div>
--- src/components/Credential.tsx
+++ src/components/Credential.tsx
@@ -4,13 +4,14 @@ import styled from 'styled-components'
 import { chat, colors } from '../theme'
 import Utils from './Utils'
 
-import { ICredentialNode, ICredentialValue, IJobNode } from './Types'
+import { ICredentialNode, ICredentialValue, IJobNode, JobStatus } from './Types'
 import AcceptableJob from './AcceptableJob'
+import { Checkmark } from 'grommet-icons'
 
 type IProps = { credential: ICredentialNode; job: IJobNode }
 
 const P = styled(Paragraph)`
-  padding: 9px 17px 11px 0px;
+  padding: 3px 17px 3px 0px;
   margin: 0;
   font-size: ${chat.fontSize};
   color: ${colors.smallText};
@@ -21,36 +22,59 @@ const Content = styled(Box)`
   padding: ${chat.contentPadding};
   margin: 0;
   display: inline-block;
+  width: 100%;
 `
-const Strong = styled.strong`
-  color: ${colors.chatText};
-  overflow-x: scroll;
+const Line = styled(Paragraph)`
+  text-align: center;
+  border-bottom: 1px solid ${colors.chatBorder};
+  line-height: 0.1em;
+  width: 100%;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
 `
 
 const Span = styled.span`
-  color: ${colors.smallText};
+  color: ${colors.brand};
+`
+const ValueSpan = styled(Span)`
+  color: ${colors.selected};
+  padding: 0 0 0 0.5rem;
 `
 
+// todo add declined credential
 function Credential({ credential, job }: IProps) {
   return (
     <AcceptableJob job={job} canAccept={true}>
       <Content>
-        <P>
-          Credential Offer:{' '}
-          <Strong>{Utils.parseSchemaName(credential.schemaId)}</Strong>
+        <P style={{ fontWeight: 'bold' }}>
+          Credential Offer{' '}
+          {job.status === JobStatus.COMPLETE && (
+            <Checkmark color={colors.selected} size="16px" />
+          )}
         </P>
-        {credential.attributes.map((item: ICredentialValue) => {
-          return (
-            <div key={item.id}>
-              <Box pad="0 0 0 0" direction="row">
-                <Span>{item.name}:</Span>
-                <Box pad="0 0 0 0.5rem">
-                  <strong>{item.value}</strong>
+        <Line></Line>
+        <P>{Utils.parseSchemaName(credential.schemaId)}</P>
+        <Box pad="0 0 10px 0">
+          {credential.attributes.map((item: ICredentialValue) => {
+            return (
+              <div key={item.id}>
+                <Box pad="0 20px" direction="row">
+                  <Span>{item.name}:</Span>
+                  <ValueSpan>{item.value}</ValueSpan>
                 </Box>
-              </Box>
-            </div>
-          )
-        })}
+              </div>
+            )
+          })}
+        </Box>
+        <Line></Line>
+        {job.status === JobStatus.COMPLETE && (
+          <Box>
+            <P>
+              Received this credential{' '}
+              {Utils.toDateDotString(credential.issuedMs!)}
+            </P>
+          </Box>
+        )}
       </Content>
     </AcceptableJob>
   )
