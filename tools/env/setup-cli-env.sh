da03e5e066b487348e89e0c31659d6ed57f9f88b
Harri Lainio <harri.lainio@op.fi>
2021-06-16 15:36:28 +0300

    use-key.sh added and absolut path

--- tools/env/setup-cli-env.sh
+++ tools/env/setup-cli-env.sh
@@ -2,9 +2,10 @@
 
 if [ -z "$FCLI_KEY" ]; then
 	export FCLI_KEY=`cli new-key`
+	printf "export FCLI_KEY=%s" $FCLI_KEY > use-key.sh
 	echo "$FCLI_KEY" >> .key-backup
 fi
-export FCLI_TLS_PATH="./config/cert"
+export FCLI_TLS_PATH="$PWD/config/cert"
 export FCLI_USER="findy-root"
 export FCLI_URL=http://localhost:8088
 export FCLI_SERVER=localhost:50052
