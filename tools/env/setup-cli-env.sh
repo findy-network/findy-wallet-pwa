a93c1b1c4d36b7cfcc657c2243a44d76b328c608
Harri Lainio <harri.lainio@op.fi>
2021-06-17 13:57:23 +0300

    not used cli variable for calls

--- tools/env/setup-cli-env.sh
+++ tools/env/setup-cli-env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cli=${FCLI:-findy-agent-cli}
+
 if [ -z "$FCLI_KEY" ]; then
-	export FCLI_KEY=`cli new-key`
+	export FCLI_KEY=`$cli new-key`
 	printf "export FCLI_KEY=%s" $FCLI_KEY > use-key.sh
 	echo "$FCLI_KEY" >> .key-backup
 fi
@@ -10,4 +12,5 @@ export FCLI_USER="findy-root"
 export FCLI_URL=http://localhost:8088
 export FCLI_SERVER=localhost:50052
 export FCLI_ORIGIN=http://localhost:3000
+export FCLI=$cli
 
