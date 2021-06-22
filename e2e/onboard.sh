436ea391624b6dd7cec055ab9ad124f5de9a0086
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-06-22 19:43:16 +0300

    Add more e2e tests.

--- e2e/onboard.sh
+++ e2e/onboard.sh
@@ -2,16 +2,45 @@
 
 timestamp=$(date +%s)
 
+user=user-$timestamp
+org=org-$timestamp
+auth_url="http://localhost:8088"
+auth_origin="http://localhost:3000"
+default_key="15308490f1e4026284594dd08d31291bc8ef2aeac730d0daf6ff87bb92d4336c"
+
+# register web wallet user
 findy-agent-cli authn register \
-    -u test-$timestamp \
-    --url http://localhost:8088 \
-    --origin http://localhost:3000 \
-    --key 15308490f1e4026284594dd08d31291bc8ef2aeac730d0daf6ff87bb92d4336c
+    -u $user \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key
 
+# login web wallet user
 jwt=$(findy-agent-cli authn login \
-    -u test-$timestamp \
-    --url http://localhost:8088 \
-    --origin http://localhost:3000 \
-    --key 15308490f1e4026284594dd08d31291bc8ef2aeac730d0daf6ff87bb92d4336c)
+    -u $user \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key)
 
-echo {\"jwt\": \"$jwt\", \"user\": \"test-$timestamp\"} > ./e2e/e2e.user.json
+# register org agent
+findy-agent-cli authn register \
+    -u $org \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key
+
+# login org
+org_jwt=$(findy-agent-cli authn login \
+    -u $org \
+    --url $auth_url \
+    --origin $auth_origin \
+    --key $default_key)
+
+invitation=$(findy-agent-cli agent invitation \
+    --label organisation \
+    --jwt $org_jwt \
+    --server "localhost:50052" \
+    --tls-path ./tools/env/config/cert)
+
+echo {\"jwt\": \"$jwt\", \"user\": \"$user\"} > ./e2e/e2e.user.json
+echo $invitation > ./e2e/e2e.invitation.json
--- e2e/test.js
+++ e2e/test.js
@@ -1,18 +1,53 @@
+const user = require('./e2e.user.json')
+const userCmd = `window.localStorage.token = "${user.jwt}"`
+const addConBtn = '//button[contains(.,"Add connection")]'
+
 module.exports = {
   'Check app loads': (browser) => {
     const home = 'http://localhost:3000/'
-    const addConBtn = '//button[contains(.,"Add connection")]'
     const newInvBtn = '//button[contains(.,"New invitation")]'
-    const user = require('./e2e.user.json')
-    const cmd = `window.localStorage.token = "${user.jwt}"`
     browser
       .url(home)
-      .execute(cmd)
+      .execute(userCmd)
       .url(home)
       .useXpath()
       .waitForElementVisible(addConBtn)
-      .useXpath()
       .waitForElementVisible(newInvBtn)
+      .end()
+  },
+  'Check connection is done': (browser) => {
+    const home = 'http://localhost:3000/'
+    const invitationInput = 'input[placeholder="Enter invitation code"]'
+    const confirmBtn = '//button[contains(.,"Confirm")]'
+    const organisationLabel = '//p[contains(.,"organisation")]'
+    const invitation = JSON.stringify(require('./e2e.invitation.json'))
+    browser
+      .url(home)
+      .execute(userCmd)
+      .url(home)
+      .useXpath()
+      .waitForElementVisible(addConBtn)
+      .click(addConBtn)
+      .useCss()
+      .waitForElementVisible(invitationInput)
+      .setValue(invitationInput, invitation)
+      .useXpath()
+      .waitForElementVisible(confirmBtn)
+      .click(confirmBtn)
+      .waitForElementVisible(organisationLabel)
+      .end()
+  },
+  'Check navigation works': (browser) => {
+    const home = 'http://localhost:3000/'
+    const walletLink = '//a[contains(.,"Wallet")]'
+    const credentialsHeader = '//h2[contains(.,"Your wallet is empty")]'
+    browser
+      .url(home)
+      .execute(userCmd)
+      .url(home)
+      .useXpath()
+      .click(walletLink)
+      .waitForElementVisible(credentialsHeader)
       .end()
   },
 }
