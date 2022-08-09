8c1ec63a720c2ac2b88dc08afefa3b60662de9d9
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-08-09 10:38:58 +0300

    Add options to e2e test for testing with existing user/organisation.

--- e2e/onboard.sh
+++ e2e/onboard.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+# Script configuration with env variables:
+# E2E_USER: when defined, new user onboarding is skipped
+# E2E_ORG: when defined, new organisation onboarding is skipped
+# E2E_KEY: needed when using either of latter - agent key
+# E2E_CRED_DEF_ID: when defined, cred def creation is skipped (needs E2E_ORG)
+
+# Use when testing with remote host:
+# AGENCY_URL
+# AGENCY_API_URL
+# AGENCY_TLS_PATH
+
 set -e
 
 read_timeout="60s"
 timestamp=$(date +%s)
 
-user=user-$timestamp
-org=org-$timestamp
+user=$E2E_USER
+existing="true"
+if [ -z "$user" ]; then
+  echo "User not defined, creating new..."
+  user="user-$timestamp"
+  existing="false"
+fi
+
+org=$E2E_ORG
+if [ -z "$org" ]; then
+  echo "Organisation not defined, creating new..."
+  org="org-$timestamp"
+fi
+
 bot_file="./e2e/e2e-sa.yaml"
 
 echo "::add-mask::$user"
@@ -29,17 +52,22 @@ if [ -z "$tls_path" ]; then
   tls_path="./tools/env/config/cert"
 fi
 
-default_key=$(findy-agent-cli new-key)
+default_key=$E2E_KEY
+if [ -z "$default_key" ]; then
+  default_key=$(findy-agent-cli new-key)
+fi
 
 echo "Running e2e test for $auth_url (origin: $auth_origin, api: $grpc_server)"
 
 # register web wallet user
-echo "Register user $user"
-findy-agent-cli authn register \
-    -u $user \
-    --url $auth_url \
-    --origin $auth_origin \
-    --key $default_key
+if [ -z "$E2E_USER" ]; then
+  echo "Register user $user"
+  findy-agent-cli authn register \
+      -u $user \
+      --url $auth_url \
+      --origin $auth_origin \
+      --key $default_key
+fi
 
 # login web wallet user
 echo "Login user $user"
@@ -50,12 +78,14 @@ jwt=$(findy-agent-cli authn login \
     --key $default_key)
 
 # register org agent
-echo "Register org $org"
-findy-agent-cli authn register \
-    -u $org \
-    --url $auth_url \
-    --origin $auth_origin \
-    --key $default_key
+if [ -z "$E2E_ORG" ]; then
+  echo "Register org $org"
+  findy-agent-cli authn register \
+      -u $org \
+      --url $auth_url \
+      --origin $auth_origin \
+      --key $default_key
+fi
 
 # login org
 echo "Login org $org"
@@ -69,44 +99,49 @@ org_jwt=$(findy-agent-cli authn login \
 echo "Create invitation for organisation"
 invitation=$(findy-agent-cli agent invitation \
     --tls-path $tls_path --server $grpc_server \
-    --label organisation \
+    --label $org \
     --jwt $org_jwt)
 
-echo {\"jwt\": \"$jwt\", \"user\": \"$user\"} > ./e2e/e2e.user.json
 echo $invitation > ./e2e/e2e.invitation.json
 connection_id=$(node -pe "require('./e2e/e2e.invitation.json')['@id']")
 echo "::add-mask::$connection_id"
 echo "Invitation created with connection id $connection_id"
 
-# create schema
-echo "Create schema"
-sch_id=$(findy-agent-cli agent create-schema \
-    --tls-path $tls_path --server $grpc_server \
-    --jwt $org_jwt --name="email" --version=1.0 email)
-echo "::add-mask::$sch_id"
+cred_def_id=$E2E_CRED_DEF_ID
+if [ -z "$E2E_CRED_DEF_ID" ]; then
+  # create schema
+  echo "Create schema"
+  sch_id=$(findy-agent-cli agent create-schema \
+      --tls-path $tls_path --server $grpc_server \
+      --jwt $org_jwt --name="email" --version=1.0 email)
+  echo "::add-mask::$sch_id"
 
-# read schema - make sure it's found in ledger
-echo "Read schema"
-schema=$(findy-agent-cli agent get-schema \
-    --tls-path $tls_path --server $grpc_server \
-    --jwt $org_jwt --schema-id $sch_id --timeout $read_timeout)
+  # read schema - make sure it's found in ledger
+  echo "Read schema"
+  schema=$(findy-agent-cli agent get-schema \
+      --tls-path $tls_path --server $grpc_server \
+      --jwt $org_jwt --schema-id $sch_id --timeout $read_timeout)
 
-echo "Schema read successfully: $schema"
+  echo "Schema read successfully: $schema"
 
-# create cred def
-echo "Create cred def with schema id $sch_id"
-cred_def_id=$(findy-agent-cli agent create-cred-def \
-    --tls-path $tls_path --server $grpc_server \
-    --jwt $org_jwt --id $sch_id --tag $org)
-echo "::add-mask::$cred_def_id"
+  # create cred def
+  echo "Create cred def with schema id $sch_id"
+  cred_def_id=$(findy-agent-cli agent create-cred-def \
+      --tls-path $tls_path --server $grpc_server \
+      --jwt $org_jwt --id $sch_id --tag $org)
+  echo "::add-mask::$cred_def_id"
 
-# read cred def - make sure it's found in ledger
-echo "Read cred def"
-cred_def=$(findy-agent-cli agent get-cred-def \
-    --tls-path $tls_path --server $grpc_server \
-    --jwt $org_jwt --id $cred_def_id --timeout $read_timeout)
+  # read cred def - make sure it's found in ledger
+  echo "Read cred def"
+  cred_def=$(findy-agent-cli agent get-cred-def \
+      --tls-path $tls_path --server $grpc_server \
+      --jwt $org_jwt --id $cred_def_id --timeout $read_timeout)
 
-echo "Cred def read successfully: $cred_def"
+  echo "Cred def read successfully: $cred_def"
+fi
+
+# store details for testing
+echo {\"jwt\": \"$jwt\", \"user\": \"$user\", \"existing\": $existing, \"organisation\": \"$org\", \"key\": \"$default_key\", \"credDefId\": \"$cred_def_id\" } > ./e2e/e2e.user.json
 
 # store cred def id to bot template
 cat "$bot_file".template > "$bot_file"
